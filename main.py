186e866041b710c28261cc7bc79b781ec8dcdd8d
EvSDv <vyacheslav.eyhe@gmail.com>
2019-02-25 11:16:20 +0900

    Доработка замечаний

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def create_db():
     with pg.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD)as conn:
         with conn.cursor() as cur:
             cur.execute("""
-                CREATE TABLE Student (
-                id INTEGER PRIMARY KEY NOT NULL,
+                CREATE TABLE IF NOT EXISTS Student (
+                id SERIAL PRIMARY KEY NOT NULL,
                 name VARCHAR(100) NOT NULL,
                 gpa NUMERIC(10,2),
                 birth TIMESTAMP WITH TIME ZONE);
@@ -18,17 +18,17 @@ def create_db():
 
         with conn.cursor() as cur:
             cur.execute("""
-                CREATE TABLE Course (
-                id INTEGER PRIMARY KEY NOT NULL,
+                CREATE TABLE IF NOT EXISTS Course (
+                id SERIAL PRIMARY KEY NOT NULL,
                 name VARCHAR(100) NOT NULL);
                 """)
 
         with conn.cursor() as cur:
             cur.execute("""
-                CREATE TABLE student_course (
+                CREATE TABLE IF NOT EXISTS student_course (
                 id serial PRIMARY KEY,
-                student_id INTEGER REFERENCES Student(id),
-                course_id INTEGER REFERENCES Course(id));
+                student_id INTEGER REFERENCES Student(id) ON DELETE CASCADE,
+                course_id INTEGER REFERENCES Course(id) ON DELETE CASCADE);
                 """)
 
 
@@ -36,8 +36,9 @@ def add_student(student):
     with pg.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD)as conn:
         with conn.cursor() as cur:
             cur.execute("""
-                INSERT INTO Student (id,name, gpa, birth) values (%s, %s, %s, %s);
-                """, (student.get('id'), student.get('name'), student.get('gpa'), student.get('birth')))
+                INSERT INTO Student (name, gpa, birth) values (%s, %s, %s) RETURNING id;
+                """, (student['name'], student['gpa'], student['birth']))
+            return cur.fetchone()[0]
 
 
 def get_student(student_id):
@@ -48,7 +49,7 @@ def get_student(student_id):
             FROM Student
             WHERE id = %s ;
             """, str(student_id))
-            return cur.fetchall()
+            return cur.fetchone()[0]
 
 
 def get_students(course_id):  # возвращает студентов определенного курса
@@ -66,12 +67,13 @@ def add_students(course_id, students):  # создает студентов и #
     for student in students:
         cur = conn.cursor()
         cur.execute("""
-            INSERT INTO Student (id,name, gpa, birth) values (%s, %s, %s, %s);
-            """, (student.get('id'), student.get('name'), student.get('gpa'), student.get('birth')))
+            INSERT INTO Student (name, gpa, birth) values (%s, %s, %s) RETURNING id;
+            """, (student['name'], student['gpa'], student['birth']))
+        id_new_student = cur.fetchone()[0]
         cur = conn.cursor()
         cur.execute("""
             insert into student_course (student_id, course_id) values (%s, %s)
-            """, (student.get('id'), course_id))  # добавить связь студент-курс
+            """, (id_new_student, course_id))  # добавить связь студент-курс
     conn.commit()
     conn.close()
 
@@ -80,22 +82,23 @@ def add_cours(cours):  # добавляет курс
     with pg.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD)as conn:
         with conn.cursor() as cur:
             cur.execute("""
-                INSERT INTO Course (id,name) values (%s, %s);
-                """, (cours.get('id'), cours.get('name')))
+                INSERT INTO Course (name) values (%s);
+                """, (cours['name'],))
 
 
 if __name__ == '__main__':
     create_db()
-    student = {'id': 1, 'name': 'Иванов Иван Иванович', 'gpa': 8,
+    student = {'name': 'Иванов Иван Иванович',
+               'gpa': 8,
                'birth': '1987-09-07'}  # студент для теста add_student()
     add_student(student)
-    print(get_student(1))  # тест get_student()
-    add_cours({'id': 1, 'name': 'Нетология'})  # Тест add_cours()
+    print(get_student('1'))  # тест get_student()
+    add_cours({'name': 'Нетология'})  # Тест add_cours()
 
-    students = [{'id': 2, 'name': 'Batman Bin Superman', 'gpa': 3, 'birth': '1980-05-12'},
-                {'id': 3, 'name': 'Андреев Андрей Андреевич', 'gpa': 4, 'birth': '1982-04-16'},
-                {'id': 4, 'name': 'Михайлов Михаил Михайлович', 'gpa': 5, 'birth': '1990-08-07'},
-                {'id': 5, 'name': 'Антошкин Антон Антонович', 'gpa': 7, 'birth': '1993-01-18'}]
+    students = [{'name': 'Batman Bin Superman', 'gpa': 3, 'birth': '1980-05-12'},
+                {'name': 'Андреев Андрей Андреевич', 'gpa': 4, 'birth': '1982-04-16'},
+                {'name': 'Михайлов Михаил Михайлович', 'gpa': 5, 'birth': '1990-08-07'},
+                {'name': 'Антошкин Антон Антонович', 'gpa': 7, 'birth': '1993-01-18'}]
 
     add_students(1, students)  # Тест add_students()
     print(get_students(1))
